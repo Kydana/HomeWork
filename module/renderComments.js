cba5dcb132da49aeaaad2710ac26b4724269be8a
Kydana <nkggirl@mail.ru>
2026-04-24 18:48:13 +0900

    домашка 4я

---
+++ module/renderComments.js
@@ -0,0 +1,34 @@
+import { comments } from "./comments.js";
+import { initLikeListeners, initReplyListeners } from "./init.js";
+
+// const textarea = document.querySelector(".add-form-text");
+
+export const renderComments = () => {
+    const list = document.querySelector(".comments");
+
+    list.innerHTML = comments.map((comment, index) => {
+        return `
+            <li class="comment" data-index="${index}">
+            <div class="comment-header">
+                <div>${comment.name}</div>
+                <div>${comment.date.toLocaleDateString()}</div>
+            </div>
+            <div class="comment-body">
+                <div class="comment-text">
+                    ${comment.text}
+                </div>
+            </div>
+            <div class="comment-footer">
+                <div class="likes">
+                    <span class="likes-counter">${comment.likes}</span>
+                    <button data-index="${index}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
+                </div>
+            </div>
+        </li>
+        `;
+    })
+    .join("");
+
+   initLikeListeners(renderComments);
+   initReplyListeners();
+};
