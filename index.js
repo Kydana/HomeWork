d72ee628c4d8ceff31464174bd078e54bfbaf936
Kydana <nkggirl@mail.ru>
2026-06-24 12:51:14 +0900

    домашка 6

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { updateComments } from './module/comments.js';
 import { initAddCommentListener } from './module/init.js'
 import { renderComments } from './module/renderComments.js'
 
+document.querySelector('.comments').innerHTML = 'Подожди дожди дождии...'
+
 fetchComments().then(data => {
     updateComments(data)
     renderComments()
